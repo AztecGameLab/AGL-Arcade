85b9d0d67b8a71253fe471e58d39e7d8e7971f0e
Spectrum_Programmer <justin.adams1015@gmail.com>
2025-05-26 20:20:49 -1000

    GameData and Canvas Fixes

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,23 @@
+using TriInspector;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[HideMonoScript]
+[CreateAssetMenu(menuName = "GameData")]
+public class GameData : ScriptableObject
+{
+    [Header("Images")]
+    
+    [Tooltip("The image that is shown when the game is not installed.")]
+    public Sprite locked;
+    [Tooltip("The image that is shown when the game is installed.")]
+    public Sprite unlocked;
+
+    [Header("Other")] 
+    
+    [Tooltip("Description for the game.")]
+    public string description;
+    [Tooltip("URL to the game's Itch.io download page.")]
+    public string url;
+
+}
